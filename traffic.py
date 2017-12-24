03a90e407cadd2d432b58df33bf9e95f80076180
root <root@localhost.localdomain>
2017-12-24 11:16:56 -0500

    first commit

---
+++ traffic.py
@@ -0,0 +1,100 @@
+import pcap
+import dpkt
+
+#cap = pcap.pcap('eth3')
+#cap.setfilter('tcp port 80')
+#files4out = {}
+#url = 'www.baidu.com'
+
+class Traffic(object):
+    def __init__(self,eth='eth0',filter=''):
+        self.eth = eth
+        self.filter = filter
+        self.cap = pcap.pcap(self.eth)
+        self.cap.setfilter(self.filter)
+        self.files4out = {}
+
+    def setfind(self,content):
+        self.findContent = content
+
+    def getTraffic(self):
+        for ptime, pktdata in self.cap:
+            pkt = dpkt.ethernet.Ethernet(pktdata)
+            if pkt.data.data.__class__.__name__ <> 'TCP':
+                continue
+            ipsrc_tag = 0
+            ipdst_tag = 0
+            sport_tag = 0
+            dport_tag = 0
+
+            ipdata = pkt.data
+            sip = '%d.%d.%d.%d' % tuple(map(ord, list(ipdata.src)))
+            dip = '%d.%d.%d.%d' % tuple(map(ord, list(ipdata.dst)))
+
+            tcpdata = pkt.data.data
+            sport = tcpdata.sport
+            dport = tcpdata.dport
+
+            src_tag = sip
+            dst_tag = dip
+            sp_tag = str(sport)
+            dp_tag = str(dport)
+            if ord(list(ipdata.src)[0]) > ord(list(ipdata.dst)[0]):
+                temp = dst_tag
+                dst_tag = src_tag
+                src_tag = temp
+
+            if sport > dport:
+                temp = sp_tag
+                sp_tag = dp_tag
+                dp_tag = temp
+
+            content = self.findContent
+            FLAG = 0
+
+            appdata = tcpdata.data
+            if appdata.find(content) <> -1:
+                FLAG = 1
+
+            name = src_tag + '_' + dst_tag + '_' + sp_tag + '_' + dp_tag
+            if (name) in self.files4out:
+                item = self.files4out[name]
+                fi = 0
+                cnt = item[1]
+                if cnt < 6 and item[3] <> 1:
+                    item[1] += 1
+                    item[2].append(pktdata)
+                    if FLAG == 1:
+                        item[3] = 1
+                elif item[3] == 1:
+                    for index in range(cnt + 1):
+                        pktdatai = item[2][index]
+                        pkti = dpkt.ethernet.Ethernet(pktdatai)
+                        ipdatai = pkti.data
+                        tcpdatai = pkti.data.data
+                        sipi = '%d.%d.%d.%d' % tuple(map(ord, list(ipdatai.src)))
+                        dipi = '%d.%d.%d.%d' % tuple(map(ord, list(ipdatai.dst)))
+                        sporti = tcpdatai.sport
+                        dporti = tcpdatai.dport
+                        print '[datai]' + sipi + ':' + str(sporti) + '-' + dipi + ':' + str(dporti)
+                    item[1] = -1
+
+                    print '[data]' + sip + ':' + str(sport) + '-' + dip + ':' + str(dport)
+                else:
+                    del self.files4out[name]
+            else:
+                item = [0, 0, [], 0, 0]
+                item[2].append(pktdata)
+                self.files4out[name] = item
+
+
+def main():
+     traffic = Traffic('eth0','tcp')
+     traffic.setfind('\"id\":1,\"jsonrpc\":\"2.0\"')
+     traffic.getTraffic()
+
+
+if __name__=="__main__":
+    main()
+
+
